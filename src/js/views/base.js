d412332c61d51988fcf723f70d696db7e4d5a02c
Elly Pham <ellypham@gmail.com>
2020-05-06 19:38:48 -0400

    Add .read__list element

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,6 +3,7 @@ export const elements = {
   searchInput: document.querySelector('.search__field'),
   searchResult: document.querySelector('.search__result'),
   comicResults: document.querySelector('.comics__results'),
-  comicPages: document.querySelector('.comics__pages')
+  comicPages: document.querySelector('.comics__pages'),
+  readList: document.querySelector('.read__list')
 
 }
