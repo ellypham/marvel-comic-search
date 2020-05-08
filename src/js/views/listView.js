34638f7ca18d779c6e4ed12468fad53d3d4e90ea
Elly Pham <ellypham@gmail.com>
2020-05-07 20:55:21 -0400

    Add deleteItem function

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -15,3 +15,8 @@ export const renderItem = item => {
 
   elements.readList.insertAdjacentHTML('beforeend', markup);
 }
+
+export const deleteItem = id => {
+  const item = document.querySelector('[data-itemid="${id}"]');
+  if(item) item.parentElement.removeChild(item);
+}
