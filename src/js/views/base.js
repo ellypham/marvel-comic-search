ce6bd84bfb01916f9e53679d36a3fa7e5d279a5c
ellypham <elly.pham@glossier.com>
2020-05-10 13:34:37 -0400

    Add new query selector

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,9 +1,9 @@
 export const elements = {
-  searchForm: document.querySelector('.search'),
-  searchInput: document.querySelector('.search__field'),
-  searchResult: document.querySelector('.search__result'),
-  comicResults: document.querySelector('.comics__results'),
-  comicPages: document.querySelector('.comics__pages'),
-  readList: document.querySelector('.read__list')
-
-}
+  searchForm: document.querySelector(".search"),
+  characterList: document.querySelector(".featured-characters__list"),
+  searchInput: document.querySelector(".search__field"),
+  searchResult: document.querySelector(".search__result"),
+  comicResults: document.querySelector(".comics__results"),
+  comicPages: document.querySelector(".comics__pages"),
+  readList: document.querySelector(".read__list"),
+};
