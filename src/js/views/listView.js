da5c9d87e7af3cd9aa36fda5f6a1aded2dc61bf7
ellypham <elly.pham@glossier.com>
2020-05-10 17:30:09 -0400

    Add functions to hide and show reading panel

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -4,13 +4,25 @@ export const clearResults = () => {
   elements.readList.innerHTML = "";
 };
 
+export const showReadList = () => {
+  elements.readPanel.style.width = "50%";
+  console.log("show read list");
+};
+
+export const hideReadList = () => {
+  elements.readPanel.style.width = "0%";
+  console.log("hide read list");
+};
+
 export const renderItem = (item) => {
   const markup = `
     <li class='read__item' data-itemid=${item.id}>
       <input type='checkbox' id='read'/>
       <img src=${item.img} />
       <p>${item.title}</p>
-      <button class='btn__delete'>delete</button>
+      <button class='btn__delete'>
+        <i class="fas fa-times fa-2x"></i>
+      </button>
     </li>
   `;
 
