b8b21660778f417d41b3bc8c9ef63b8ac17694e0
ellypham <elly.pham@glossier.com>
2020-05-10 17:31:08 -0400

    Add new elements to list

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,9 +1,13 @@
 export const elements = {
   searchForm: document.querySelector(".search"),
-  characterList: document.querySelector(".featured-characters__list"),
+  readListBtn: document.querySelector(".btn__read-list"),
+  feature: document.querySelector(".featured-characters"),
+  featureList: document.querySelector(".featured-characters__list"),
   searchInput: document.querySelector(".search__field"),
   searchResult: document.querySelector(".search__result"),
   comicResults: document.querySelector(".comics__results"),
   comicPages: document.querySelector(".comics__pages"),
   readList: document.querySelector(".read__list"),
+  readPanel: document.querySelector(".read__panel"),
+  panelCloseBtn: document.querySelector(".btn__close-read-panel"),
 };
